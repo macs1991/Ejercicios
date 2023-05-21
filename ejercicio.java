80977fef8d74d144451ac589f605ce854b67f372
Mario <macs0219@gmail.com>
2023-05-21 11:49:41 +0200

    commit inicial

---
+++ ejercicio.java
@@ -0,0 +1,17 @@
+public class ejercicio {
+    
+    public static void main(String[] args) {
+        int a=4;
+        int b=2;
+        int resultado =0;
+        resultado = suma( a, b);
+        System.out.println(resultado);
+
+        
+    }
+
+    public static int suma(int a, int b) {
+        return a+b;
+
+    }
+}
